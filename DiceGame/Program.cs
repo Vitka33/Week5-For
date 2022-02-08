4b6aa84a7ddee793350d233307ff39575c1e4061
Viktor Maljuga <vmaljuga@tktk.ee>
2022-02-08 21:23:50 +0200

    add new projects

---
+++ DiceGame/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mängus osaleb 2 mängijat - arvuti (cpu) ja kasutaja (user);
+            //mõlemad mängijad viskavad täringuid;
+            //programm kontrollib, kumb mängija viskas rohkem
+            //mängija, kes viskab rohkem, ongi mängu võitja
+            //*täringuid visatakse kolm korda
+            //programm kuulutab võitjat.
+
+            int sum1 = 0;
+            int sum2 = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                Random rnd = new Random();
+                //arvuti vise
+                int cpuRandom = rnd.Next(1, 7);
+                //kasutaja vise
+                int userRandom = rnd.Next(1, 7);
+
+                int cpuScore = 0;
+                int userScore = 0;
+
+                Console.WriteLine($"Arvuti viskas {cpuRandom}. Kasutaja viskas {userRandom}");
+
+                if (cpuRandom < userRandom)
+                {
+                    Console.WriteLine("Kasutaja on mängu võitnud. Palju õnne!");
+                    userScore = userScore + 1;
+                }
+                else if (cpuRandom > userRandom)
+                {
+                    Console.WriteLine("Arvuti on mängu võitnud.");
+                    cpuScore = cpuScore + 1;
+                }
+                else
+                {
+                    Console.WriteLine("Viik!");
+                }
+                sum1 = sum1 + userScore;
+                sum2 = sum2 + cpuScore;
+            }
+            Console.WriteLine($"User wins {sum1} times.");
+            Console.WriteLine($"Cpu wins {sum2} times");
+
+            if(sum1 > sum2)
+            {
+                Console.WriteLine("User is the winner!");
+            }
+            else
+            {
+                Console.WriteLine("Cpu is the winner!");
+            }
+
+
+        }
+    }
+}
---
+++ OROperator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OROperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada oma kasutaja tunnust ja salasõna
+            //kui sisestatud kasutajatunnus on "admin" ja sisestatud salasõna on "admin1234"
+            //siis konsooliss kuvatakse "Tere tuelmast!";
+            //muul juhul konsoolis kuvatakse "Vale kasutajatunnus või salasõna. Proovi uuesti."
+
+            //Or ||
+            //true || true --> true
+            //false || true --> true
+            //true || false --> true
+            //false || false --> false
+
+
+            Console.WriteLine("Sisesta kasutajatunnus:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Sisesta salasõna:");
+            string userPassword = Console.ReadLine();
+
+            if(userName != "admin" || userPassword != "admin1234")
+            {
+                Console.WriteLine("Vale kasutajatunnus või salasõna. Proovi uuesti.");
+            }
+            else
+            {
+                Console.WriteLine("Tere tulemat!"); 
+            }
+        }
+    }
+}
